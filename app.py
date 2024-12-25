7657581fbebaa44d8cb4e4395f9e03470884ad5e
unknown-guy123 <gitearyan01@gmail.com>
2024-12-25 13:40:24 +0530

    added database connection

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request, render_template
+import psycopg2
+
+app = Flask(__name__)
+
+# Database configuration
+DD_URL = 'postgresql://postgres:123456789@localhost:5432/manishafashion'
+
+# Connect to the database
+def connect():
+    return psycopg2.connect(DD_URL)
+
+if __name__ == '__main__':
+    app.run(debug=True)
